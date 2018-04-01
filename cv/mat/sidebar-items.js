d85fd541e353626b2940910477624e8723afccf1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-04-01 00:48:22 +0000

    Deploy nebgnahz/cv-rs to github.com/nebgnahz/cv-rs.git:gh-pages

---
+++ cv/mat/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["Mat","The class `Mat` represents an n-dimensional dense numerical single-channel or multi-channel array. It can be used to store real or complex-valued vectors and matrices, grayscale or color images, voxel volumes, vector fields, point clouds, tensors, histograms"]]});
